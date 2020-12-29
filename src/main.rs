7d620665a6c4498b233f0111bd40fe775cdd1d90
Aravind Kumar <arvizard@hotmail.com>
2020-12-29 10:35:10 +0800

    some fixes here to get the serial to be sent.

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,26 @@ fn main() {
     let port_name = "/dev/tty.usbmodem14701";
     let baud_rate = 9600;
     let port = serialport::new(port_name, baud_rate)
-    .timeout(Duration::from_secs(5))
+    .timeout(Duration::from_millis(500))
     .open();
 
 match port {
     Ok(mut port) => {
-        
-
-        let mut serial_buf: Vec<u8> = vec![0; 1000];
+        let mut serial_buf = [0; 1];
+        const END: u8 = 90;
         println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
-        
         loop {
-            
-            match port.read(serial_buf.as_mut_slice()) {
+            match port.read(&mut serial_buf) {
                 Ok(t) => 
                 {
+                    if &serial_buf[0] == &END {break};
+                    
                     io::stdout().write_all(&serial_buf[..t]).unwrap();
                 }
                 ,
-                Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {println!("d");},
+                Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {
+                    port.write("g".as_bytes()).expect("Send failed");
+                },
                 Err(e) => eprintln!("{:?}", e),
             }
         }
